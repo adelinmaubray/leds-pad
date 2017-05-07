622695fee4342c259981894ddafb648e141d7299
Adelin <adelin@maubray.be>
2017-05-07 13:42:01 +0200

    Ajout fonction Snake

--- app/src/main/java/com/example/adelin/ledspad/Snake.java
+++ app/src/main/java/com/example/adelin/ledspad/Snake.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -20,81 +18,35 @@ public class Snake extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snake);
         Log.i("romrom", "value:" + MainActivity.btSocket);
-        try {
-
-            MainActivity.btSocket.getOutputStream().write("2\n".getBytes());
-            MainActivity.btSocket.getOutputStream().flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void onBackPressed() {
-
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), "Pause Menu", Toast.LENGTH_SHORT).show();
+        try {
+            MainActivity.btSocket.getOutputStream().write("s\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "Resume", Toast.LENGTH_SHORT).show();
-    }
-
-    public void goUp(View view) {
         try {
-
-            MainActivity.btSocket.getOutputStream().write("u\n".getBytes());
+            MainActivity.btSocket.getOutputStream().write("s\n".getBytes());
             MainActivity.btSocket.getOutputStream().flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Toast.makeText(getApplicationContext(), "Up", Toast.LENGTH_SHORT).show();
     }
 
-    public void goDown(View view) {
-        try {
-
-            MainActivity.btSocket.getOutputStream().write("d\n".getBytes());
-            MainActivity.btSocket.getOutputStream().flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getApplicationContext(), "Down", Toast.LENGTH_SHORT).show();
-    }
-
-    public void goLeft(View view) {
-        try {
-
-            MainActivity.btSocket.getOutputStream().write("l\n".getBytes());
-            MainActivity.btSocket.getOutputStream().flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getApplicationContext(), "Left", Toast.LENGTH_SHORT).show();
-    }
-
-    public void goRight(View view) {
-        try {
-
-            MainActivity.btSocket.getOutputStream().write("r\n".getBytes());
-            MainActivity.btSocket.getOutputStream().flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getApplicationContext(), "Right", Toast.LENGTH_SHORT).show();
-    }
-
-    public void pause(View view) {
-        Toast.makeText(getApplicationContext(), "Pause", Toast.LENGTH_SHORT).show();
-        startActivityForResult(new Intent(this, Pause.class), 1000);
-    }
-
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1000) {
             if (resultCode == 1) {
@@ -104,5 +56,49 @@ public class Snake extends AppCompatActivity {
         }
     }
 
+    public void goUp() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("u\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void goDown() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("d\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goLeft() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("l\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goRight() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("r\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pause() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("s\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+            startActivityForResult(new Intent(this, Pause.class), 1000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
