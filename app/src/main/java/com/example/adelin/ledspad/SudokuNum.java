ed869de17341aaca5a935449700d3cdfa4448610
Adelin <adelin@maubray.be>
2017-05-07 13:45:41 +0200

    Ajout fonctions SudokuNum

--- app/src/main/java/com/example/adelin/ledspad/SudokuNum.java
+++ app/src/main/java/com/example/adelin/ledspad/SudokuNum.java
@@ -2,8 +2,8 @@ package com.example.adelin.ledspad;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
+
+import java.io.IOException;
 
 /**
  * Created by Adelin on 28/04/2017.
@@ -19,103 +19,132 @@ public class SudokuNum extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), "Pause Menu", Toast.LENGTH_SHORT).show();
+        try {
+            MainActivity.btSocket.getOutputStream().write("s\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "Resume", Toast.LENGTH_SHORT).show();
-    }
-
-
-    public void openSudoku(View view) {
-        SudokuNum.this.finish();
-    }
-
-    public void sendNum() {
-        //send num to wall
-
-        //retourner a sudoWin
-    }
-
-    public void sendOne(View view) {
-        Toast.makeText(getApplicationContext(), "1 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendTwo(View view) {
-        Toast.makeText(getApplicationContext(), "2 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendThree(View view) {
-        Toast.makeText(getApplicationContext(), "3 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendFour(View view) {
-        Toast.makeText(getApplicationContext(), "4 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendFive(View view) {
-        Toast.makeText(getApplicationContext(), "5 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendSix(View view) {
-        Toast.makeText(getApplicationContext(), "6 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendSeven(View view) {
-        Toast.makeText(getApplicationContext(), "7 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendEight(View view) {
-        Toast.makeText(getApplicationContext(), "8 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendNine(View view) {
-        Toast.makeText(getApplicationContext(), "9 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    public void sendZero(View view) {
-        Toast.makeText(getApplicationContext(), "0 sent", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-
-    //tentative de chnager la couleur quand on clique
-
-    /*final Button ok_button = (Button) findViewById(R.id.ok_button);
-    final Button back_button = (Button) findViewById(R.id.back_button);
-
-ok_button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View arg0) {
-// TODO Auto-generated method stub
-            back_button.setBackgroundColor(Color.CYAN);
+        try {
+            MainActivity.btSocket.getOutputStream().write("s\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    });
+    }
 
-back_button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View arg0) {
-// TODO Auto-generated method stub
-            ok_button.setBackgroundColor(Color.RED);
+
+    public void openSudoku() {
+        finish();
+    }
+
+    public void sendZero(){
+        try {
+            MainActivity.btSocket.getOutputStream().write("0\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    };
-*/
+    }
+
+    public void sendOne() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("1\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendTwo() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("2\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendThree() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("3\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendFour() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("4\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendFive() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("5\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendSix() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("6\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendSeven() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("7\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendEight() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("8\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+
+    public void sendNine() {
+        try {
+            MainActivity.btSocket.getOutputStream().write("9\n".getBytes());
+            MainActivity.btSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
 
 }
